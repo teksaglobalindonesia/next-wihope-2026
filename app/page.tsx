35ab322de11f9febbffd44a2b9595390213a2eb7
Rai Steeven <ngurahraiw1ll1am@gmail.com>
2026-05-21 14:53:36 +0800

    Change Name

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <p>Hello world</p>;
+  return <p className="text-4xl">Hello <span className="font-bold bg-slate-800 bg-clip-text text-transparent">Steeven</span></p>;
 }
