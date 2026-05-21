5ec0042aa727b1298ef310108ff6d6694bc4014e
Dery Yahya <yeahdery@gmail.com>
2026-05-21 14:53:56 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
   return (
-    <div className="flex items-center justify-center h-screen">
-      <button className="bg-gray-950 text-white w-[100px] h-[60px] rounded-lg hover:bg-gray-500">Hello Dery</button>
+    <div className="flex items-center justify-center h-screen bg-[url('https://i.pinimg.com/736x/3d/d9/5b/3dd95bc48b5de4db9fd6f33c244635d5.jpg')] bg-cover bg-center">
+      <button className="bg-gray-950 text-white w-[100px] h-[60px] rounded-lg hover:bg-gray-100 hover:text-black hover:scale-105 transition-all">Hello Dery</button>
     </div>
   );
 }
