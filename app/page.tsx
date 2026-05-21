796cfa56422225ab9b490200c7b6cdc134ad0f6e
Widhi Mahaputra <widhimp@gmail.com>
2026-05-21 18:18:17 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,22 @@
 import ProductList from "@/sections/product-list";
 
+export const dynamic = "force-dynamic";
+
 async function getProducts() {
     try {
-        const response = await fetch("https://fakestoreapi.com/products");
+    const response = await fetch("https://fakestoreapi.com/products", {
+      cache: "no-store",
+    });
 
         if(!response.ok) {
             throw new Error("Failed to fetch data dari API");
         }
 
-        return response.json(); 
-        
+        return response.json();
+
     } catch (error) {
         console.log(error);
+    return [];
     }
 }
 
