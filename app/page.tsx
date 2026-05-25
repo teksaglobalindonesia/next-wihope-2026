0cfa18d8110f297293c204f2c43ef6891c2fd950
Logan Diminique <gadingmatahari2@gmail.com>
2026-05-25 22:34:29 +0800

    update

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import Community from '@/components/custom/community';
 import { CustButton } from '@/components/custom/CustButton';
 import Header from '@/components/custom/header';
 import Hero from '@/components/custom/hero';
+import HowToDesign from '@/components/custom/how-to-design';
 import OurClients from '@/components/custom/our-clients';
 import UnseenSpending from '@/components/custom/unseen-spending';
 import { Button } from '@/components/ui/button';
@@ -16,6 +17,7 @@ export default function Page() {
       <Community />
       <UnseenSpending />
       <Achievement />
+      <HowToDesign />
     </div>
   );
 }
