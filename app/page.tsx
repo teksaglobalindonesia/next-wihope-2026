8eaf10b7796336652ca2baa06171d33dca9400bd
putrimhrni2503 <putrimahatari000@gmail.com>
2026-05-25 17:12:49 +0800

    hero.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Header from "@/components/custom/header";
+import Hero from "@/components/custom/hero";
 
 export default function Page() {
   return(
+    <>
     <Header />
+    <Hero />
+    </>
   ) ;
     
 }
---
+++ components/custom/hero.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+export default function Hero() {
+  return (
+
+    <section className="w-full h-[599px] flex justify-center items-center px-[144px] py-[96px] gap-[104px] bg-neutral-silver"> 
+
+      <div className="flex flex-col w-[657px] h-[276px] gap-[32px] justify-center ">
+
+        <div className="flex flex-col gap-4">
+          <h1 className="font-inter text-[64px] font-semibold leading-[76px] text-neutral-d-grey">
+            Lessons and insights <br />
+            <span className="text-brand-primary">
+              from 8 years
+            </span>
+          </h1>
+
+          <p className="font-inter text-[16px] font-normal leading-[24px] text-neutral-grey">
+            Where to grow your business as a photographer: site or social media?
+          </p>
+        </div>
+
+        <button className="font-inter w-[128px] h-[52px] rounded-[4px] bg-brand-primary px-[32px] py-[14px] text-[16px] font-medium leading-[24px] text-white transition-all duration-300 hover:bg-shade-3">
+          Register
+        </button>
+
+      </div>
+
+      <div className="flex flex-1 justify-center items-center">
+        <Image src="/illustration.svg" alt="hero image" width={391} height={407} />
+      </div>
+
+    </section>
+  );
+}
