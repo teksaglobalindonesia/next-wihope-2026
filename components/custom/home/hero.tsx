a14acfc529e013f4d54dc2289df9b55c458102ab
High-Tower1 <putuendra180@gmail.com>
2026-05-25 16:56:22 +0800

    hero image test

--- components/custom/home/hero.tsx
+++ components/custom/home/hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
 
                 <div>
                     <Image
-                        src="/illustrations/illustration-1.png"
+                        src="/illustrations/illustration-2.png" 
                         width={391}
                         height={407}
                         alt="Illustration"
