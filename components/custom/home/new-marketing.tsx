7650d08dd985b93460315c5106923c981115b821
High-Tower1 <putuendra180@gmail.com>
2026-05-26 17:57:50 +0800

    fix deploy 3

--- components/custom/home/new-marketing.tsx
+++ components/custom/home/new-marketing.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Blogs } from "@/lib/data-dummy"
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 
 export default function NewMarketing() {
     return (
@@ -32,7 +32,7 @@ export default function NewMarketing() {
                             <div className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2 z-10 flex flex-col justify-center items-center text-center bg-neutral-white gap-4 p-4 rounded-[8px] shadow-[0px_8px_16px_0px_#ABBED166] w-[317px]">
                                 <span className="text-neutral-grey text-xl font-semibold">{blog.title}</span>
                                 <Button className="text-brand-primary text-xl font-semibold" variant="standardTertiary" size="clear" asChild>
-                                    <Link href={blog.href}>Read More <ArrowRightIcon/></Link>
+                                    <Link href={blog.href}>Read More <ArrowRight/></Link>
                                 </Button>
                             </div>
                         </div>
--- components/custom/home/ribbon.tsx
+++ components/custom/home/ribbon.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { ArrowRightIcon } from "lucide-react";
+import { ArrowRight } from "lucide-react";
 import Link from "next/link";
 
 export default function Ribbon() {
@@ -8,7 +8,7 @@ export default function Ribbon() {
             <div className="flex flex-col justify-center items-center text-center gap-8 py-8 w-full bg-neutral-silver">
                 <span className="text-neutral-black text-[64px] leading-[76px] font-semibold w-[887px]">Pellentesque suscipit fringilla libero eu.</span>
                 <Button className="" variant="standardPrimary" size="medium" asChild>
-                    <Link href="/register">Get a Demo <ArrowRightIcon/></Link>
+                    <Link href="/register">Get a Demo <ArrowRight/></Link>
                 </Button>
 
             </div>
