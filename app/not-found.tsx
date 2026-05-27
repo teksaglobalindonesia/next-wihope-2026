1efe9a6724821b688de2ed056006463af6f5b81f
Dery Yahya <yeahdery@gmail.com>
2026-05-28 00:01:23 +0800

    back to home dari not found beneran back to home

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -24,7 +24,7 @@ export default function NotFound() {
           Go back
         </Button>
         <Button
-          onClick={() => router.back()}
+          onClick={() => router.push("/")}
           variant="ghost"
           size="lg"
         >
