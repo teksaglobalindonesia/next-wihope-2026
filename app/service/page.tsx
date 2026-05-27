410a24fbb7b50829f70b452bd93347916e24739e
Rai Steeven <ngurahraiw1ll1am@gmail.com>
2026-05-27 21:49:23 +0800

    1. Menambahkan file dan folder baru yaitu service/page.tsx dan simple-hero.tsx
    2. menampilkan simple hero pada service/page.tsx

---
+++ app/service/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/custom/header";
+import SimpleHero from "@/components/custom/simple-hero";
+
+export default function Page (){
+    return (
+        <>
+        <Header/>
+        <SimpleHero/>
+        </>
+    )
+}
---
+++ components/custom/simple-hero.tsx
@@ -0,0 +1,8 @@
+export default function SimpleHero (){
+    return (
+        <div className="w-full h-[308px] bg-neutral-silver flex flex-col justify-center items-center font-inter py-[96px] mt-[84px]">
+            <p className="font-semibold text-[64px] text-neutral-d_grey">Our <span className="text-brand-primary">Service</span></p>
+            <p className="text-neutral-d_grey text-[16px] mt-[8px]">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet justo ipsum.</p>
+        </div>
+    )
+}
