7df12c997157699715524e9b0eabcc76e2431053
nadyayascikirani <nadyayascikirani@gmail.com>
2026-05-28 14:16:16 +0800

    fix: button casing import

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -7,7 +7,7 @@ import { PanelLeft } from 'lucide-react';
 
 import { useIsMobile } from '@/hooks/use-mobile';
 import { cn } from '@/lib/utils';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Separator } from '@/components/ui/separator';
 import { Sheet, SheetContent } from '@/components/ui/sheet';
--- components/ui/table/data-table.tsx
+++ components/ui/table/data-table.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { Button } from '@/components/ui/Button';
+import { Button } from '@/components/ui/button';
 import { ScrollArea, ScrollBar } from '@/components/ui/scroll-area';
 import {
   Select,
