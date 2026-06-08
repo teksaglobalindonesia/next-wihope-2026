62c15213b4f13f9a13281d729a91c7b0db1539e0
Rai Steeven <ngurahraiw1ll1am@gmail.com>
2026-06-08 15:54:56 +0800

    1. menambahkan animasi rotate pada community section dan sedikit memperbaiki animasi pada our-client

--- components/custom/community.tsx
+++ components/custom/community.tsx
@@ -1,39 +1,61 @@
+'use client';
 import Image from 'next/image';
 import communityData from '@/lib/community-data';
+import { useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
-    export default function Community() {
-    return (
-        <div className="flex h-[416px] w-full flex-col px-[50px] items-center mt-[25px]">
-        <div className="flex flex-col items-center">
-            <p className="text-center font-inter text-[36px] font-semibold text-neutral-d_grey">
-            Manage your entire community <br /> in a single system
-            </p>
-            <p className="mt-[8px] font-inter text-[16px] text-neutral-d_grey">
-            Who is Nextcent suitable for?
-            </p>
-        </div>
-        <div className="flex justify-between mt-[16px] gap-[85px]">
-            {communityData.map((data, id) => (
-            <div
-                key={id}
-                className="h-[284px] w-[299px]  bg-white shadow-[0px_2px_4px_0px_#ABBED133] rounded-[8px] py-[24px] flex flex-col items-center px-[28px] hover:scale-[1.05] hover:shadow-lg transition-all ease-in-out duration-500 "
-            >
-                <div className=" w-[81px] h-[41px] flex justify-center relative">
-                <div className="rounded-bl-[5px] rounded-br-[10px] rounded-tl-[18px] rounded-tr-[5px] bg-tint-tint_5 w-[50px] h-[49px] rotate-180">
-                </div>
-                <Image src={`${data.img}.png`} width={48} height={48} alt="" quality={100} className="absolute left-0 bottom-0" />
-                </div>
-
-                <p className="text-center font-inter text-[28px] font-bold text-neutral-d_grey mt-[16px]">
-                {data.judul}
-                </p>
-                <p className="font-inter text-[14px] text-neutral-d_grey text-center mt-[8px]">
-                {data.text}
-                </p>
-
+export default function Community() {
+  useEffect(() => {
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: '.box',
+        start: 'top 0%',
+        toggleActions: 'play none none none'
+      }
+    });
+    tl.from('.card0', { rotate: 30, duration: 0.5 })
+      .from('.card1', { rotate: 30, duration: 0.5 })
+      .from('.card2', { rotate: 30, duration: 0.5 });
+  }, []);
+  return (
+    <div className="box mt-[25px] flex h-[416px] w-full flex-col items-center px-[50px]">
+      <div className="flex flex-col items-center">
+        <p className="text-center font-inter text-[36px] font-semibold text-neutral-d_grey">
+          Manage your entire community <br /> in a single system
+        </p>
+        <p className="mt-[8px] font-inter text-[16px] text-neutral-d_grey">
+          Who is Nextcent suitable for?
+        </p>
+      </div>
+      <div className="mt-[16px] flex justify-between gap-[85px]">
+        {communityData.map((data, id) => (
+          <div
+            key={id}
+            className={`card${id} flex h-[284px] w-[299px]  flex-col items-center rounded-[8px] bg-white px-[28px] py-[24px] shadow-[0px_2px_4px_0px_#ABBED133] `}
+          >
+            <div className=" relative flex h-[41px] w-[81px] justify-center">
+              <div className="h-[49px] w-[50px] rotate-180 rounded-bl-[5px] rounded-br-[10px] rounded-tl-[18px] rounded-tr-[5px] bg-tint-tint_5"></div>
+              <Image
+                src={`${data.img}.png`}
+                width={48}
+                height={48}
+                alt=""
+                quality={100}
+                className="absolute bottom-0 left-0"
+              />
             </div>
-            ))}
-        </div>
-        </div>
-    );
-    }
+
+            <p className="mt-[16px] text-center font-inter text-[28px] font-bold text-neutral-d_grey">
+              {data.judul}
+            </p>
+            <p className="mt-[8px] text-center font-inter text-[14px] text-neutral-d_grey">
+              {data.text}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
--- components/custom/our-client.tsx
+++ components/custom/our-client.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import { gsap } from 'gsap';
 import { useEffect } from 'react';
-import {ScrollTrigger} from 'gsap/ScrollTrigger';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
 
 export default function Client() {
@@ -16,33 +16,29 @@ export default function Client() {
     '/Logo-images/Logo (7).png'
   ];
 
-  
-
   useEffect(() => {
     const tl = gsap.timeline({
       scrollTrigger: {
-    trigger: '.box',
-    start: 'top -40%',
-    end: 'top 20%',
-    toggleActions: 'play none none none',
-  }
-});
+        trigger: '.box',
+        start: 'top 50%',
+        end: 'top 20%',
+        toggleActions: 'play none none none'
+      }
+    });
 
     tl.from('.judul', { y: 100, opacity: 0, duration: 1 })
       .from('.text', { y: 100, opacity: 0, duration: 1 })
-      .from('.icon0', {scale:0, duration:0.5})
-      .from('.icon1', {scale:0, duration:0.5})
-      .from('.icon2', {scale:0, duration:0.5})
-      .from('.icon3', {scale:0, duration:0.5})
-      .from('.icon4', {scale:0, duration:0.5})
-      .from('.icon5', {scale:0, duration:0.5})
-      .from('.icon6', {scale:0, duration:0.5})
+      .from('.icon0', { scale: 0, duration: 0.5 })
+      .from('.icon1', { scale: 0, duration: 0.5 })
+      .from('.icon2', { scale: 0, duration: 0.5 })
+      .from('.icon3', { scale: 0, duration: 0.5 })
+      .from('.icon4', { scale: 0, duration: 0.5 })
+      .from('.icon5', { scale: 0, duration: 0.5 })
+      .from('.icon6', { scale: 0, duration: 0.5 });
   }, []);
 
- 
-
   return (
-    <div className="h-[270] w-full bg-white py-[40px]">
+    <div className="box h-[270] w-full bg-white py-[40px]">
       <div className="flex flex-col items-center">
         <div className="flex flex-col items-center gap-[8px]">
           <p className="judul font-inter text-[36px] font-semibold text-neutral-d_grey ">
