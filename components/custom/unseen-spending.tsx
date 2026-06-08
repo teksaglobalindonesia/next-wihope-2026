6908f952a66cd7d4fce94b6a07190bbbc2e35435
Rai Steeven <ngurahraiw1ll1am@gmail.com>
2026-06-08 16:43:43 +0800

    1. menambahkan animasi pada gambar, judul, dan teks pada section unseen-spending

--- components/custom/unseen-spending.tsx
+++ components/custom/unseen-spending.tsx
@@ -1,11 +1,20 @@
 'use client';
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import Button from '../Function/button';
+import gsap from 'gsap';
 
 export default function UnseenSpending() {
   const [loadingButton, setLoadingButton] = useState<string | null>(null);
+  const [active, setActive] = useState(false);
+  const tl = gsap.timeline();
+
+  useEffect(() => {
+    tl.to('.foto', { scale: active? 1.15 : 1, duration: 0.5 }, 0)
+      .to('.judul', { x:active? 40 : 0, duration: 0.5 }, 0)
+      .to('.text', { x:active? -40 : 0, duration: 0.5 }, 0);
+  }, [active]);
 
   const handleButtonClick = (buttonName: string) => {
     setLoadingButton(buttonName);
@@ -15,7 +24,7 @@ export default function UnseenSpending() {
     }, 2000);
   };
   return (
-    <div className="mt-[76px] flex h-[529px] w-full justify-center bg-white py-[48px] gap-[56px]">
+    <div className="mt-[76px] flex h-[529px] w-full justify-center gap-[56px] bg-white py-[48px]">
       <div className="">
         <Image
           src="/Illustration/Illustration-2.png"
@@ -23,13 +32,14 @@ export default function UnseenSpending() {
           height={329}
           quality={100}
           alt=""
+          className='foto'
         />
       </div>
-      <div className="flex flex-col w-[600px] h-[300px]">
-        <p className="font-inter text-[36px] font-semibold text-neutral-d_grey">
+      <div className="flex h-[300px] w-[600px] flex-col">
+        <p className="judul font-inter text-[36px] font-semibold text-neutral-d_grey">
           The unseen of spending three years at Pixelgrade
         </p>
-        <p className="font-inter text-[14px] text-neutral-d_grey mt-[16px] mb-[32px]">
+        <p className="text mb-[32px] mt-[16px] font-inter text-[14px] text-neutral-d_grey">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet
           justo ipsum. Sed accumsan quam vitae est varius fringilla.
           Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
@@ -42,7 +52,7 @@ export default function UnseenSpending() {
           className="h-[52px] w-[151px] rounded-[4px]"
           loading={loadingButton === 'learn more'}
           disabled={loadingButton !== null && loadingButton !== 'learn more'}
-          onClick={() => handleButtonClick('learn more')}
+          onClick={() => {handleButtonClick('learn more'); setActive(!active)}}
         >
           Learn More
         </Button>
