085b7c6c8c609c14c8b5aa3dfb00804fb2cde6a6
nadyayascikirani <nadyayascikirani@gmail.com>
2026-06-08 17:15:15 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,25 @@
-import Community from '@/components/custom/community';
+import Hero from '@/components/custom/hero';
+import Header from '@/components/custom/header';
 import OurClients from '@/components/custom/our-clients';
-import UnsenSpending from '@/components/custom/unseen-spending';
+import Community from '@/components/custom/community';
+import UnseenSpending from '@/components/custom/unseen-spending';
 import Achievement from '@/components/custom/achievement';
 import HowToDesign from '@/components/custom/how-to-design';
 import NewMarketing from '@/components/custom/new-marketing';
 import Ribbon from '@/components/custom/ribbon';
 import Footer from '@/components/custom/footer';
-import Hero from '@/components/custom/hero';
-import Header from '@/components/custom/header';
+
 
 export default function Page() {
   return (
     <>
       <Header/>
       <Hero />
-      <OurClients />
+      <OurClients/>
       <Community />
-      <UnsenSpending />
+      <UnseenSpending/>
       <Achievement />
-      <HowToDesign />
+      <HowToDesign/>
       <NewMarketing />
       <Ribbon />
       <Footer />
